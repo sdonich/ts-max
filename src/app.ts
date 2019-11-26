33c5fc7bd2124d64293cf67bb7c692378fb78ffa
Sergei <sdonich@gmail.com>
2019-11-26 16:10:38 +0200

    nc

--- src/app.ts
+++ src/app.ts
@@ -190,6 +190,14 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
 
 class ProjectItem extends Component<HTMLUListElement, HTMLLinkElement> {
 	private project: Project;
+
+	get persons() {
+		if (this.project.people === 1) {
+			return '1 person';
+		} else {
+			return `${this.project.people} persons`;
+		}
+	}
 	
 	constructor(hostId: string, project: Project) {
 		super('single-project', hostId, false, project.id);
@@ -204,7 +212,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLinkElement> {
 	renderContent() {
 		this.element.querySelector('h2')!.textContent = this.project.title;
 		this.element.querySelector('p')!.textContent = this.project.description;
-		this.element.querySelector('h3')!.textContent = this.project.people.toString();
+		this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
 	}
 }
 
